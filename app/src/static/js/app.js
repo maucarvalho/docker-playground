6d24890a6f26d3cd632baddb380e345680667e61
maucarvalho <mauriciocarvalhosl@gmail.com>
2021-12-17 11:35:13 -0300

    changing add button

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                         {submitting ? 'Adding...' : 'Adicionar'}
+                         {submitting ? 'Adding...' : 'Add New'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
